8dbc22836f6cb6e28a796e439597ff2ed1b65a68
BezawadaHaritha <69068526+BezawadaHaritha@users.noreply.github.com>
2020-08-31 22:31:08 +0530

    Add files via upload

---
+++ day2.js
@@ -0,0 +1,23 @@
+//program to search for a particular charectar in a string
+
+// let name="knowledge";
+// let index= name.indexOf("w")
+// console.log(index);
+
+// program to convert minutes to seconds
+// let minute=5;
+// let seconds=minute*60;
+// console.log(seconds);
+
+// program to search for an element in an array of strings
+ //let array1=["air","water","land","fire","earth"];
+// console.log(array1[2]);
+
+// program to display only elements containing 'a' in an array
+// for(let i=0;i<array1.length;i++){
+//     if(array1[i].includes("a")){
+//         console.log(array1[i]);
+//     }
+// }
+//print an array in reverse order
+//console.log(array1.reverse());
