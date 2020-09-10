e146e1be2b4631ab69503a6e2445c461d437102e
BezawadaHaritha <69068526+BezawadaHaritha@users.noreply.github.com>
2020-09-10 23:53:37 +0530

    Add files via upload

---
+++ q3.js
@@ -0,0 +1,29 @@
+let details = [
+      {
+        name: "Virat",
+        age: 30,
+        country: "India",
+        hobbies: "Cricket",
+    
+      },
+      {
+        name: "PV Sindu",
+        age: 28,
+        country: "India",
+        hobbies: "Badminton",
+      },
+      {
+        name: "Leionel Messi",
+        age: 43,
+        country: "America",
+        hobbies: "football",
+      },
+    ];
+    
+    for (let i = 0; i < details.length; i++) {
+      console.log(details[i]);
+    }
+
+    
+
+    
