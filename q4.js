eea9d795c04f582ebdb3c3f42c2d88d1e2f4a27a
BezawadaHaritha <69068526+BezawadaHaritha@users.noreply.github.com>
2020-09-11 10:46:45 +0530

    Add files via upload

---
+++ q4.js
@@ -0,0 +1,32 @@
+let details = [
+  {
+    name: "Virat",
+    age: 30,
+    country: "India",
+    hobbies: "Cricket",
+  },
+  {
+    name: "PV Sindu",
+    age: 28,
+    country: "India",
+    hobbies: "Badminton",
+  },
+  {
+    name: "Leionel Messi",
+    age: 43,
+    country: "America",
+    hobbies: "football",
+  },
+];
+for (i = 0; i <= details[i].length; i++) {
+  if (details[i].age < 30) {
+    console.log(details[i]);
+  }
+}
+for (i = 0; i <= details[i]; i++) {
+  if (details[i].country == India) {
+    console.log(details[i]);
+  }
+}
+
+
